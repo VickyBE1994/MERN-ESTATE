97a9b69f73541c73d788d8d045ef7a35b8ec428b
VickyBE1994 <vickyvip81@gmail.com>
2024-09-22 02:08:50 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
-//import cors from "cors"
+import path from "path"
 import cookieParser from 'cookie-parser'
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
@@ -12,7 +12,7 @@ dotenv.config()
 
 const app = express()
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 app.use(cookieParser())
 
 
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 console.log(error);
 
 })
-
+const __dirname = path.resolve()
 app.listen(8000,()=>{
     console.log('server listning port 8000');
     
@@ -32,6 +32,12 @@ app.listen(8000,()=>{
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
 app.use("/api/listing",listingRouter)
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
