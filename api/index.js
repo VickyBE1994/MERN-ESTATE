80d5d367807823e08011c9d4a8897c0771395a9c
VickyBE1994 <vickyvip81@gmail.com>
2024-09-23 00:18:30 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,50 +1,48 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import path from "path"
-import cookieParser from 'cookie-parser'
-import userRouter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
-import listingRouter  from "./routes/listing.route.js"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
-dotenv.config()
+dotenv.config();
 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
+app.use(cookieParser());
 
-
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("connected to mongoDb");
-    
-}).catch((error)=>{
-console.log(error);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+const __dirname = path.resolve();
+app.listen(8000, () => {
+  console.log("server listning port 8000");
+});
 
-})
-const __dirname = path.resolve()
-app.listen(8000,()=>{
-    console.log('server listning port 8000');
-    
-})
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-app.use("/api/user",userRouter)
-app.use("/api/auth",authRouter)
-app.use("/api/listing",listingRouter)
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
-app.get('*',(req,res)=>{
-res.sendFile(path.join(__dirname,'client','dist','index.html'))
-})
-
-app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error"
-    return res.status(statusCode).json({
-        succuss:false,
-        statusCode,
-        message,
-    })
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    succuss: false,
+    statusCode,
+    message,
+  });
+});
